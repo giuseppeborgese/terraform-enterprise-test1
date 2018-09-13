f5944cb013ce755a3cd50ccb87ad6043a13337aa
giuseppeborgese <giuseppeborgese@gmail.com>
2018-09-13 15:10:44 +0200

    add a rule

--- sg.tf
+++ sg.tf
@@ -10,3 +10,15 @@ resource "aws_security_group" "firstsg" {
     }
 }
 */
+
+/*
+resource "aws_security_group_rule" "httpin" {
+  description       = "HTTP"
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.firstsg.id}"
+}
+*/    
